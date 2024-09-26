78057b9b69ca0dca21ff3a7d1672794989d31118
Leonardo Brum <leonardobrum.dev@gmail.com>
2024-09-26 09:22:11 -0300

    feat: add donations and volunteers page to routes and header

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { ContactComponent } from './pages/contact/contact.component';
+import { DonationsComponent } from './pages/donations/donations.component';
 import { HomeComponent } from './pages/home/home.component';
+import { VolunteersComponent } from './pages/volunteers/volunteers.component';
 
 export const routes: Routes = [
   {
@@ -15,6 +17,16 @@ export const routes: Routes = [
   {
     path: 'contact',
     component: ContactComponent
+
+  },
+  {
+    path: 'donations',
+    component: DonationsComponent
+
+  },
+  {
+    path: 'volunteers',
+    component: VolunteersComponent
 
   }
 ];
