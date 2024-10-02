56cbed7133c26db3a1c98ba549f54f2e4f775204
Leonardo Brum <leonardobrum.dev@gmail.com>
2024-10-02 19:12:53 -0300

    feat: add volunteers page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { DonationsComponent } from './pages/donations/donations.component';
 import { HomeComponent } from './pages/home/home.component';
 import { SoonPageComponent } from './pages/soon-page/soon-page.component';
+import { VolunteersComponent } from './pages/volunteers/volunteers.component';
 
 export const routes: Routes = [
   {
@@ -28,6 +29,6 @@ export const routes: Routes = [
   },
   {
     path: 'help/volunteers',
-    component: SoonPageComponent
+    component: VolunteersComponent
   }
 ];
